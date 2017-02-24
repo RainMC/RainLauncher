41a8ee44bb3c0ca4bb1465b5a0142ebf87ce5011
rex.guo <rex.guo@elitesland.com>
2017-02-24 09:50:37 +0800

    added empty files

--- src/main/java/com/rainmc/RainLauncher/App.java
+++ src/main/java/com/rainmc/RainLauncher/App.java
@@ -1,10 +1,16 @@
 package com.rainmc.RainLauncher;
 
-public class main {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+public class App extends Application {
     public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
         System.out.println("[APP] Application startup...");
         System.out.println("[APP] Application started.");
     }
-
 }
