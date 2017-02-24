8ea38a7b3ddd2ee6aa5f52e01ec07d7b14d1b566
trexguo <t.rex.guo@gmail.com>
2017-02-24 23:48:34 +0800

    gradle

--- src/main/java/com/rainmc/RainLauncher/App.java
+++ src/main/java/com/rainmc/RainLauncher/App.java
@@ -1,6 +1,9 @@
 package com.rainmc.RainLauncher;
 
+import com.jfoenix.controls.JFXDecorator;
+import com.jfoenix.svg.SVGGlyphLoader;
 import javafx.application.Application;
+import javafx.scene.*;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -9,8 +12,17 @@ public class App extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage stage) throws Exception {
+        Group root = new Group();
+        Scene scene = new Scene(root);
+
+        //		stage.initStyle(StageStyle.UNDECORATED);
+        //		stage.setFullScreen(true);
+        stage.setMinWidth(800);
+        stage.setMinHeight(600);
+        stage.setScene(scene);
+        stage.show();
         System.out.println("[APP] Application startup...");
-        System.out.println("[APP] Application started.");
     }
+
 }
